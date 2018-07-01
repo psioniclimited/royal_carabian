79a99be86b4a43edeea3eaf96d9bcd36007352bc
Raihan Zaman <raihan.zaman@amadeyr.org>
2018-07-01 06:52:42 +0600

    newly added the landing page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
+import { LandingPage } from '../pages/landing/landing';
+
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    LandingPage
+    
   ],
   imports: [
     BrowserModule,
@@ -19,7 +21,8 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    LandingPage
+    
   ],
   providers: [
     StatusBar,
