3f0d9d5be416caa8d7e116903bc06507a497c3f0
Raihan Zaman <raihan.zaman@amadeyr.org>
2018-07-11 06:22:23 +0600

    added the array in the embarkation ts file

--- src/components/embarkation/embarkation.ts
+++ src/components/embarkation/embarkation.ts
@@ -13,10 +13,15 @@ import { Component } from '@angular/core';
 export class EmbarkationComponent {
 
   text: string;
+  constant_data:{title:string, time:string, image:string, description:string}[] =[
+    {"title":"PRE BOARDING", "time":"10:30 AM","image":"assets/background/embrkicon1.png","description":"Lorem ipsum dolor sit amet,  maxime modi non pariatur recusandae? Ex explicabo facere laborum."},
+    {"title":"BOARDING", "time":"12:30 PM","image":"assets/background/embrkicon1.png","description":"Lorem ipsum dolor sit amet,  maxime modi non pariatur recusandae? Ex explicabo facere laborum."},
+    {"title":"LAST CALL", "time":"02:00 PM","image":"assets/background/embrkicon1.png","description":"Lorem ipsum dolor sit amet,  maxime modi non pariatur recusandae? Ex explicabo facere laborum."},
+    {"title":"SAILING", "time":"90 MINUTES BEFORE SAILING","image":"assets/background/embrkicon1.png","description":"Lorem ipsum dolor sit amet,  maxime modi non pariatur recusandae? Ex explicabo facere laborum."}
+  ];
 
   constructor() {
-    console.log('Hello EmbarkationComponent Component');
-    this.text = 'Hello World';
+
   }
 
 }
